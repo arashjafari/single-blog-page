f7f5ebdaae0f7b7067ae45b8c0d8b76859d746e9
Arash Jafari <arash.jafari@gmail.com>
2022-01-07 02:47:07 +0330

    Adds tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.{vue,js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
