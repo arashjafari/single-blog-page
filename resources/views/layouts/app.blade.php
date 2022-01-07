3b06b87b1e247528ee3e7e97438c0086d9cb7239
Arash Jafari <arash.jafari@gmail.com>
2022-01-08 03:10:39 +0330

    Updates header style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,24 +17,34 @@
 
 <body class="bg-gray-100">
 
-
-    <div class="w-3/4 mx-auto my-0 shadow-xl">
-        <div class="h-52 relative bg-neutral-400">
-
-            <h2 class="font-bold text-3xl leading-tight absolute ml-12 top-2/4">
-                {{ $header ?? '' }}
-            </h2>
+    <div class="h-80 max-w-screen-2xl overflow-hidden">
+        <div class="header-bg bottom-0 top-0 opacity-50 h-80 -z-10 fixed -left-2/4 -right-2/4 ease-in-out">
         </div>
-
-        <div class="bg-white">
-            <div class="px-24 py-6 text-justify">
-                {{ $content ?? '' }}
-            </div>
+        <div class="header-bg header-bg-2 bottom-0 top-0 opacity-50 h-80 -z-10 fixed -left-2/4 -right-2/4 ease-in-out">
+        </div>
+        <div class="header-bg header-bg-3 bottom-0 top-0 opacity-50 h-80 -z-10 fixed -left-2/4 -right-2/4 ease-in-out">
         </div>
     </div>
 
-    <div id="app">
-        {{ $slot }}
+    <div class="-mt-80 w-full h-60">
+        <div class="w-3/4 mx-auto my-0 shadow-xl">
+            <div class="h-52 relative bg-teal-600">
+
+                <h2 class="font-bold text-3xl leading-tight absolute ml-12 top-2/4 text-gray-800">
+                    {{ $header ?? '' }}
+                </h2>
+            </div>
+
+            <div class="bg-white">
+                <div class="px-24 py-6 text-justify">
+                    {{ $content ?? '' }}
+                </div>
+            </div>
+        </div>
+
+        <div id="app">
+            {{ $slot }}
+        </div>
     </div>
 </body>
 
