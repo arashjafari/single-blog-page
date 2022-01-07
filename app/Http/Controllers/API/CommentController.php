4fde73b4ea5913ef306351c68d78f3fc487ba2ad
Arash Jafari <arash.jafari@gmail.com>
2022-01-07 03:49:01 +0330

    Adds comment controller

---
+++ app/Http/Controllers/API/CommentController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CommentRequest;
+use App\Http\Resources\CommentResource;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+    public function index($postId)
+    {
+        return response()->json([
+            'comments' => CommentResource::collection(
+                Comment::where('post_id', $postId)->get()
+            ),
+        ]);
+    }
+    
+    public function store(CommentRequest $request)
+    {
+        Comment::create([$request->validated()]);
+
+        return response()->json(['message' => 'Comment created successfully'], 201);
+    }
+}
