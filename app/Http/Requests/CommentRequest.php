49d9abb8eaceae64eaa30eb3e392502dbebe57e2
Arash Jafari <arash.jafari@gmail.com>
2022-01-07 03:47:40 +0330

    Adds comment request

---
+++ app/Http/Requests/CommentRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:3|max:25',
+            'body' => 'required|string|min:5|max:255',
+            'post_id' => 'required|integer',
+            'parent_id' => 'nullable|integer',
+        ];
+    }
+}
