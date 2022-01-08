af73c8144cd7670f4b5403835d83117fcc267e8d
Arash Jafari <arash.jafari@gmail.com>
2022-01-08 21:38:59 +0330

    Adds loading spin

--- resources/js/composables/comments.js
+++ resources/js/composables/comments.js
@@ -4,13 +4,17 @@ import axios from "axios";
 export default function useComments() {
     const comments = ref([]) 
     const errors = ref('')
+    const loading = ref(false)
 
     const getComments = async (post_id) => {
+        loading.value = true
         let response = await axios.get(`/api/v1/comments/${post_id}`)
         comments.value = response.data.comments; 
+        loading.value = false
     }
 
     const storeComment = async (data) => {
+        loading.value = true
         errors.value = ''
         try {
             await axios.post('/api/v1/comments', data)
@@ -19,6 +23,7 @@ export default function useComments() {
                 errors.value = e.response.data.errors
             }
         }
+        loading.value = false
     }
   
     return {
@@ -26,5 +31,6 @@ export default function useComments() {
         errors,
         getComments, 
         storeComment,
+        loading,
     }
 }
