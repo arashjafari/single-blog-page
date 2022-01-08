d8b6a728da8b616226aabae795ba33fcf8936f81
Arash Jafari <arash.jafari@gmail.com>
2022-01-08 21:38:15 +0330

    Reduces the background header on mobile devices

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,14 @@
 
 <body class="bg-gray-100">
 
-    <div class="h-80 max-w-screen-2xl overflow-hidden">
-        <div class="header-bg bottom-0 top-0 opacity-50 h-80 -z-10 fixed -left-2/4 -right-2/4 ease-in-out">
+    <div class="h-40 md:h-80 max-w-screen-2xl overflow-hidden">
+        <div class="header-bg bottom-0 top-0 opacity-50 h-40 md:h-80 -z-10 fixed -left-2/4 -right-2/4 ease-in-out">
         </div>
-        <div class="header-bg header-bg-2 bottom-0 top-0 opacity-50 h-80 -z-10 fixed -left-2/4 -right-2/4 ease-in-out">
+        <div
+            class="header-bg header-bg-2 bottom-0 top-0 opacity-50 h-40 md:h-80 -z-10 fixed -left-2/4 -right-2/4 ease-in-out">
         </div>
-        <div class="header-bg header-bg-3 bottom-0 top-0 opacity-50 h-80 -z-10 fixed -left-2/4 -right-2/4 ease-in-out">
+        <div
+            class="header-bg header-bg-3 bottom-0 top-0 opacity-50 h-40 md:h-80 -z-10 fixed -left-2/4 -right-2/4 ease-in-out">
         </div>
     </div>
 
